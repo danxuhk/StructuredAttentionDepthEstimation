41ca912b5a87e1c381973a5859b6b88a8a004c42
Dan Xu <danxuhk@gmail.com>
2018-08-12 21:34:11 +0100

    adding training details

--- StructuredAttentionDepthEstimation/test.sh
+++ StructuredAttentionDepthEstimation/test.sh
@@ -2,9 +2,9 @@
 clear
 echo $"Start testing ..."
 model_defition_path="./prototxt/test_SAN.prototxt"
-model_weights="./models/SAN_1_iter_20000.caffemodel"
-pred_output_save="./output/SAN_1_iter_20000.npy"
-kitti_data_root="/scratch/local/ssd/danxu/KITTI/kitti_raw_data/"
+model_weights="./models/SAN_deploy.caffemodel"
+pred_output_save="./output/SAN_results.npy"
+kitti_data_root="/path/to/KITTI/kitti_raw_data/"
 prediction_layer_blob_name="final_output"
 if_save_depth="False"
 gpu_id=1
--- StructuredAttentionDepthEstimation/train.py
+++ StructuredAttentionDepthEstimation/train.py
@@ -100,6 +100,6 @@ if __name__ == '__main__':
     #args = parser.parse_args()
     solver = "./prototxt/solver.prototxt"
     snapshot = ''
-    pretrained_model = "./models/pretrained_models/ResNet-50-model.caffemodel"
-    gpus = [0,2]
+    pretrained_model = "./models/pretrained_models/ResNet-50-pratrained-model.caffemodel"
+    gpus = [0,1]
     train(solver, pretrained_model, snapshot, gpus)
